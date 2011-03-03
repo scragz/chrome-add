7f13459cc0a04a557411cdbd58fb6097219b309c
Jason Scragz <scragz@imotioncreative.com>
2011-03-03 00:37:31 -0800

    0.2 based on user feedback
    added "strict" mode to only countdown while focused
    added confirm
    made sites textarea a reasonable size

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (function() {
 /* begin scragz' GM utility functions */
+var log = function(msg) { console.log(msg); }
 var _gt = function(e) { return document.getElementsByTagName(e); };
 var _gi = function(e) { return document.getElementById(e); };
 var _ce = function(e) { return document.createElement(e); };
@@ -54,13 +55,20 @@ var addGlobalStyle = function(css)
 function retryUntilTagExists(tag, success)
 {
     var tries = 0;
+	if (arguments.size > 2) var ensure = arguments[2];
+	else var ensure = function(){};
     function _retryUntilTagExists(){
         var element = _gt(tag)[0];
         if (element) {
             success(element);
+			ensure();
         } else {
             tries++;
-            if (tries < 25) window.setTimeout(go, 50);
+            if (tries < 25) {
+				window.setTimeout(_retryUntilTagExists, 100);
+			} else {
+			 	ensure();
+			}
         }
     }
     _retryUntilTagExists();
@@ -78,34 +86,65 @@ function showPage()
         html.style.display = 'block';
     });
 }
-function countdown(num)
+function countdown(delay, strict)
 {
-    num = Math.abs(parseInt(num));
+	if (confirm('Do you really want to wait to look at this site?')) {
+		focused = true;
+	} else {
+		chrome.extension.sendRequest({method: 'closeSelectedTab'}, function(){});
+		return;
+	}
+	var ms = Math.abs(parseInt(delay*1000));
+	function _countdown() {
+		//log(focused ? 'focused' : 'not focused');
+		if (focused || !strict) {
+			ms -= 1000;
+			if (ms <= 0) {
+				// you did it!
+				showPage();
+				return;
+			}
+		}
+		window.setTimeout(_countdown, 1000);
+	}
+	_countdown();
     retryUntilTagExists('title', function(title){
+		//log('found title tag');
         var text = title.innerText;
-        function _countdown()
-        {
-            if (num > 0) {
-                num = num - 1;
-                title.innerText = num+" "+text;
-                window.setTimeout(_countdown, 1000)
-            } else {
-                title.innerText = text;
-            }
+        function _countdownTitle() {
+			//log('_countdownTitle');
+			try {
+	            if (ms > 0) {
+	                title.innerText = Math.ceil(ms/1000)+" "+text;
+					window.setTimeout(_countdownTitle, 1000);
+	            } else {
+	                title.innerText = text;
+	            }
+			} catch(e){/*andwedontcare*/}
         }
-        _countdown();
+        _countdownTitle();
     });
 }
 
+var focused = false;
+function addListeners() {
+	window.addEventListener('focus', function() {
+	  focused = true;
+	});
+	window.addEventListener('blur', function() {
+	  focused = false;
+	});
+}
+
 function go(options) {
-    if (options.sites && options.delay) {
+    if (options && options.sites && options.delay) {
         var re = new RegExp('https?://('+options.sites.split("\n").join('|')+')/.*');
         var ref = new RegExp('https?://'+window.location.hostname+'/.*');
         if (re.test(window.location.href) && !ref.test(window.document.referrer)) {
             // why are you looking at this rubbish?
             hidePage();
-            window.setTimeout(showPage, options.delay*1000);
-            countdown(options.delay);
+			addListeners();
+            countdown(options.delay, options.strict);
         }
     }
 }
